903965ee61241013e55753b889adb9b6810eb8cd
Krzysztof Perzanowski <k.pezanovski@gmail.com>
2021-10-25 12:13:02 +0200

    [deploy]removed unused constants and code from helpers.js

--- src/shared/constants/helpers.js
+++ src/shared/constants/helpers.js
@@ -13,6 +13,3 @@ export const LANDINGI_LOGO = 'new-logo'
  * @type {string}
  */
 export const LANDINGI_LOGO_LONG = 'new-long-logo'
-
-export const AGENCY_DASHBOARD_SUBACCOUNTS_MIN_DATE = '2021-01-14' // That time we started collecting data
-export const AGENCY_DASHBOARD_VISITORS_MIN_DATE = '2020-12-01' // That time we started colelction data
