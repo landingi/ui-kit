413c72d9b64bb150bee39b7ded542d88204fda9a
Krzysztof Perzanowski <k.pezanovski@gmail.com>
2021-10-29 09:02:37 +0200

    Modal

--- src/shared/components/ui/Modal/Modal.test.js
+++ src/shared/components/ui/Modal/Modal.test.js
@@ -40,11 +40,42 @@ describe('<Modal /> global mount', () => {
 
   it('default prop `onClick` should be null', () => {
     const result = Modal.defaultProps.onClick()
+
+    expect(result).toBe(null)
+  })
+
+  it('default prop `onAction` should be null', () => {
+    const result = Modal.defaultProps.onAction()
+
+    expect(result).toBe(null)
+  })
+
+  it('default prop `onEdit` should be null', () => {
+    const result = Modal.defaultProps.onEdit()
+
+    expect(result).toBe(null)
+  })
+
+  it('default prop `onMarkAsSpam` should be null', () => {
+    const result = Modal.defaultProps.onMarkAsSpam()
+
     expect(result).toBe(null)
   })
 
   it('should have defined default prop isClosable', () => {
     expect(wrapper.props().isClosable).toBeDefined()
+  })
+
+  it('should have defined default prop i18n', () => {
+    expect(wrapper.props().i18n).toBeDefined()
+  })
+
+  it('should have defined default prop image', () => {
+    expect(wrapper.props().image).toBeDefined()
+  })
+
+  it('should have defined default prop isEditable', () => {
+    expect(wrapper.props().isEditable).toBeDefined()
   })
 
   it('should display backdrop when isActive', () => {
@@ -58,6 +89,14 @@ describe('<Modal /> global mount', () => {
     })
 
     expect(wrapper.find('div.modal__footer').prop('className')).toEqual('modal__footer modal__footer--align-right')
+  })
+
+  it('has loader when isLoading se to true', () => {
+    wrapper.setProps({
+      isLoading: true
+    })
+
+    expect(wrapper.find('Loader').exists()).toBe(true)
   })
 
   it('has header', () => {
@@ -83,8 +122,6 @@ describe('<Modal /> global mount', () => {
   })
 
   it('has button mark as spam in the header and triggers onMarkAsSpam callback', () => {
-    const mockonMarkAsSpam = jest.fn()
-
     wrapper.setProps({
       isMarkAsSpamVisible: true,
       onMarkAsSpam: jest.fn()
@@ -108,5 +145,71 @@ describe('<Modal /> global mount', () => {
     wrapper.find('Button').at(0).simulate('click')
 
     expect(wrapper.prop('onEdit')).toHaveBeenCalledTimes(1)
+  })
+
+  it('has component in the header', () => {
+    wrapper.setProps({
+      isComponent: true
+    })
+
+    expect(wrapper.find('div.modal__component').exists()).toBe(true)
+    expect(wrapper.find('div.modal__component').hasClass('modal__component')).toBe(true)
+  })
+
+  it('has header divider', () => {
+    wrapper.setProps({
+      hasHeaderDivider: true
+    })
+
+    expect(wrapper.find('Divider').exists()).toBe(true)
+  })
+
+  it('has not header divider', () => {
+    wrapper.setProps({
+      hasHeaderDivider: false
+    })
+
+    expect(wrapper.find('Divider').exists()).toBe(false)
+  })
+
+  it('has custom button in the footer', () => {
+    wrapper.setProps({
+      hasCustomButton: true,
+      hasFooter: true
+    })
+
+    expect(wrapper.find('div.modal__footer').exists()).toBe(true)
+    expect(wrapper.find('div.modal__footer').find('Button').at(0)).toEqual({})
+  })
+
+  it('should have button type submit when isSubmit', () => {
+    wrapper.setProps({
+      isSubmit: true,
+      hasFooter: true
+    })
+
+    expect(wrapper.find('div.modal__footer').exists()).toBe(true)
+    expect(wrapper.find('div.modal__footer').find('Button').at(1).prop('type')).toEqual('submit')
+  })
+
+  it('should have button type button when isSubmit set to false', () => {
+    wrapper.setProps({
+      isSubmit: false,
+      hasFooter: true
+    })
+
+    expect(wrapper.find('div.modal__footer').exists()).toBe(true)
+    expect(wrapper.find('div.modal__footer').find('Button').at(1).prop('type')).toEqual('button')
+  })
+
+  it('should have action icon in the action button', () => {
+    wrapper.setProps({
+      isSubmit: false,
+      hasFooter: true,
+      actionIcon: 'times'
+    })
+
+    expect(wrapper.find('div.modal__footer').exists()).toBe(true)
+    expect(wrapper.find('div.modal__footer').find('Button').at(1).find('FontAwesomeIcon').prop('icon')).toEqual('times')
   })
 })
