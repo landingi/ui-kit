8378f7daad303dbd52933b131b84029f8c70972b
Krzysztof Perzanowski <k.pezanovski@gmail.com>
2021-10-29 08:25:02 +0200

    Modal

--- src/shared/components/ui/Modal/Modal.test.js
+++ src/shared/components/ui/Modal/Modal.test.js
@@ -50,4 +50,63 @@ describe('<Modal /> global mount', () => {
   it('should display backdrop when isActive', () => {
     expect(wrapper.find(Backdrop).exists()).toBe(true)
   })
+
+  it('should align the button to the right', () => {
+    wrapper.setProps({
+      align: 'right',
+      hasFooter: true
+    })
+
+    expect(wrapper.find('div.modal__footer').prop('className')).toEqual('modal__footer modal__footer--align-right')
+  })
+
+  it('has header', () => {
+    expect(wrapper.find('div.modal__header').exists()).toBe(true)
+  })
+
+  it('has title in the header', () => {
+    wrapper.setProps({
+      i18n: {
+        title: 'some title'
+      }
+    })
+
+    expect(wrapper.find('Heading').text()).toEqual('some title')
+  })
+
+  it('has image in the header', () => {
+    wrapper.setProps({
+      image: 'http://www.landingi.com/logo.png'
+    })
+
+    expect(wrapper.find('Image').prop('src')).toEqual('http://www.landingi.com/logo.png')
+  })
+
+  it('has button mark as spam in the header and triggers onMarkAsSpam callback', () => {
+    const mockonMarkAsSpam = jest.fn()
+
+    wrapper.setProps({
+      isMarkAsSpamVisible: true,
+      onMarkAsSpam: jest.fn()
+    })
+
+    expect(wrapper.find('Button').at(0).find('FontAwesomeIcon').prop('icon')).toEqual('ban')
+
+    wrapper.find('Button').at(0).simulate('click')
+
+    expect(wrapper.prop('onMarkAsSpam')).toHaveBeenCalledTimes(1)
+  })
+
+  it('has button edit in the header and triggers onEdit callback', () => {
+    wrapper.setProps({
+      isEditable: true,
+      onEdit: jest.fn()
+    })
+
+    expect(wrapper.find('Button').at(0).find('FontAwesomeIcon').prop('icon')).toEqual('pencil-alt')
+
+    wrapper.find('Button').at(0).simulate('click')
+
+    expect(wrapper.prop('onEdit')).toHaveBeenCalledTimes(1)
+  })
 })
