54d3f6f330df8b1a1ad51294a6c92d7cb6037834
Krzysztof Perzanowski <k.pezanovski@gmail.com>
2021-11-03 23:16:44 +0100

    DropdownHead

---
+++ src/shared/components/ui/Dropdown/DropdownHead.test.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import DropdownHead from '@components/ui/Dropdown/Head'
+
+const props = {
+  children: 'some children'
+}
+
+const dropdownComponent = <DropdownHead {...props} />
+
+describe('<DropdownHead/> mount', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(dropdownComponent)
+  })
+
+  afterEach(() => {
+    wrapper.unmount()
+  })
+
+  it('is mounted', () => {
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('has children', () => {
+    expect(wrapper.find('div').text()).toEqual(wrapper.prop('children'))
+  })
+
+  it('has `dropdown__head` class', () => {
+    expect(wrapper.hasClass('dropdown__head')).toBe(true)
+  })
+})
