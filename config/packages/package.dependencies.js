aa81a3be2aadbe43110f0fc8c66b179b5457993a
Oskar Jankowiak <oskar.jankowiak@landingi.com>
2021-11-04 08:46:52 +0100

    installed new package

--- config/packages/package.dependencies.js
+++ config/packages/package.dependencies.js
@@ -21,7 +21,6 @@ module.exports = {
     "simple-flexbox": "2.3.3",
     "tiny-emitter": "2.1.0",
     "@babel/runtime": "7.15.4",
-    "react-time-picker": "4.4.3",
     "react-advanced-time-input": "1.2.0",
   },
   documentationDependencies: {
@@ -48,7 +47,6 @@ module.exports = {
     "@babel/runtime": "7.15.4",
     "docz": "2.3.1",
     "gatsby-plugin-sass": "4.14.0",
-    "react-time-picker": "4.4.3",
     "react-advanced-time-input": "1.2.0",
   },
   testingDependencies: {
@@ -74,7 +72,6 @@ module.exports = {
     "tiny-emitter": "2.1.0",
     "yup": "0.32.9",
     "@babel/runtime": "7.15.4",
-    "react-time-picker": "4.4.3",
     "react-advanced-time-input": "1.2.0",
   }
 }
