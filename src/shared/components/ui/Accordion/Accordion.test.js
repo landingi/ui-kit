d1e1947991e9100445a1c00b03d341055e9ddb59
Krzysztof Perzanowski <k.pezanovski@gmail.com>
2021-11-18 12:25:29 +0100

    [FRONT-786]Initial files - testing mugshot

--- src/shared/components/ui/Accordion/Accordion.test.js
+++ src/shared/components/ui/Accordion/Accordion.test.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import Accordion from '@components/ui/Accordion'
+import { Mugshot } from 'mugshot'
+import { WebdriverIOAdapter } from '@mugshot/webdriverio'
+import { remote } from 'webdriverio'
 
 const accordionComponent = (
   <Accordion>
@@ -21,5 +24,28 @@ describe('<Accordion/> mount', () => {
 
   it('is mounted', () => {
     expect(wrapper.exists()).toBe(true)
+  })
+})
+
+describe('<Accordion/> visual regression', () => {
+  test('GitHub project page should look the same', async () => {
+    // 1. Open the browser.
+    const browser = await remote({
+      headless: true,
+      hostname: 'localhost',
+      capabilities: { browserName: 'chrome' }
+    })
+
+    // 2. Navigate to the page you want to test.
+    await browser.url('https://github.com/NiGhTTraX/mugshot')
+
+    // 3. Set up mugshot.
+    const mugshot = new Mugshot(new WebdriverIOAdapter(browser), 'screenshots')
+
+    // 4. Take the screenshot.
+    const result = await mugshot.check('project page')
+
+    // 5. Check the result.
+    expect(result.matches).toBeTruthy()
   })
 })
