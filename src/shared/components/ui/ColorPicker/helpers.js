d1c8b5e2bed0d8b306ebf237a704b46c5c8a5018
Oskar Jankowiak <oskar.jankowiak@landingi.com>
2021-11-19 17:12:53 +0100

    added converted functions

--- src/shared/components/ui/ColorPicker/helpers.js
+++ src/shared/components/ui/ColorPicker/helpers.js
@@ -1,16 +1,16 @@
+export const hexRegex = /^#[0-9A-F]{6}$/i
+export const rgbOrRgbaRegex =
+  /^rgb(a?)[(]\s*([\d.]+\s*%?)\s*,\s*([\d.]+\s*%?)\s*,\s*([\d.]+\s*%?)\s*(?:,\s*([\d.]+)\s*)?[)]$/i
+
 /**
- * Converts hex value to rgba object
- * eg. (#345465, 1) => {
- *   r: 52
- *   g: 85
- *   b: 101
- *   a: 1
- * }
- * @param {string} hex
+ * Converts hex value to rgba object or string
+ * eg. (#345465, 1) => { r: 52, g: 85, b: 101, a: 1} | 'rgba(52,85,101,1)
+ * @param {string} hex - color value in hex
+ * @param {boolean} toString - bool param to declare if you want return string
  * @param {number} alpha
- * @returns {object} returns an object of r,g,b,a values
+ * @returns {object | string} returns an object of r,g,b,a values
  */
-export const hexToRgba = (hex, alpha = 1) => {
+export const hexToRgba = (hex, toString = false, alpha = 1) => {
   if (alpha > 1 || alpha < 0) {
     throw new Error('alpha is not correct!')
   }
@@ -18,27 +18,33 @@ export const hexToRgba = (hex, alpha = 1) => {
   const red = parseInt(hex.slice(1, 3), 16)
   const green = parseInt(hex.slice(3, 5), 16)
   const blue = parseInt(hex.slice(5, 7), 16)
-
-  return {
+  const rgbaString = `rgba(${red},${green},${blue},${alpha})`
+  const rgbaObj = {
     r: red,
     g: green,
     b: blue,
     a: alpha
   }
+
+  return toString ? rgbaString : rgbaObj
 }
 
 /**
  * Converts value from rgb string to hex value
- * eg. ''rgb(255, 255, 255)''=> #ffffff
+ * eg. ''rgb|rgba(255, 255, 255, 1)''=> #ffffff
  * @param {string} rgb
  * @returns {string}
  */
 export const rgbTohex = rgb =>
-  `#${rgb
-    .match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/)
-    .slice(1)
-    .map(n => parseInt(n, 10).toString(16).padStart(2, '0'))
-    .join('')}`
+  '#' +
+  rgb
+    .match(/[0-9|.]+/g)
+    .map((x, i) =>
+      i === 3
+        ? parseInt(255 * parseFloat(x)).toString(16)
+        : parseInt(x).toString(16)
+    )
+    .join('')
 
 /**
  *
@@ -46,11 +52,8 @@ export const rgbTohex = rgb =>
  * @returns {object} an object of red, green, blue, opacity integers
  */
 export const convertColorToObj = color => {
-  const isHexColor = /^#[0-9A-F]{6}$/i.test(color)
-  const isRGBOrRGBAColor =
-    /^rgb(a?)[(]\s*([\d.]+\s*%?)\s*,\s*([\d.]+\s*%?)\s*,\s*([\d.]+\s*%?)\s*(?:,\s*([\d.]+)\s*)?[)]$/i.test(
-      color
-    )
+  const isHexColor = hexRegex.test(color)
+  const isRGBOrRGBAColor = rgbOrRgbaRegex.test(color)
 
   if (isHexColor) {
     return hexToRgba(color)
