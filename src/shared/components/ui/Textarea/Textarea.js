a203867e86f038e39e1099bbd57c3557f6fde6d4
Oskar Jankowiak <oskar.jankowiak@landingi.com>
2021-12-13 12:49:38 +0100

    case sensitive changed textarea.scss to Textarea.scss import[deploy]

--- src/shared/components/ui/Textarea/Textarea.js
+++ src/shared/components/ui/Textarea/Textarea.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { styles } from '@helpers/css'
-import scss from './textarea.scss'
+import scss from './Textarea.scss'
 
 const cssClass = styles(scss)
 
