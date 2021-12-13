b62ddb162c0cf85286ecfff5fdb91a1d884e1241
Oskar Jankowiak <oskar.jankowiak@landingi.com>
2021-12-13 11:49:44 +0100

    added text area ui component

---
+++ src/shared/components/ui/Textarea/Textarea.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { styles } from '@helpers/css'
+import scss from './textarea.scss'
+
+const cssClass = styles(scss)
+
+/**
+ * Textarea - stateless presentational component
+ * @param {object} i18n- props with translated string
+ */
+const Textarea = ({ name, value, onChange, i18n }) => {
+  return (
+    <textarea
+      className={cssClass('textarea')}
+      name={name}
+      placeholder={i18n.placeholder}
+      onChange={onChange}
+      value={value}
+    />
+  )
+}
+
+Textarea.displayName = 'Textarea'
+
+Textarea.propTypes = {
+  name: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  i18n: PropTypes.shape({
+    placeholder: PropTypes.string
+  }).isRequired
+}
+
+export default Textarea
