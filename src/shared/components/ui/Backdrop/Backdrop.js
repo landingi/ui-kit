a8485d30b0c22133b49765e0757ca4b0b9a3c9e5
Krzysztof Perzanowski <k.pezanovski@gmail.com>
2022-01-04 13:05:47 +0100

    [EDI-885]Backdrop css-modules

--- src/shared/components/ui/Backdrop/Backdrop.js
+++ src/shared/components/ui/Backdrop/Backdrop.js
@@ -1,36 +1,24 @@
-import { styles } from '@helpers/css'
 import PropTypes from 'prop-types'
 import React from 'react'
-import scss from './Backdrop.scss'
-
-const cssClass = styles(scss)
+import styles from './Backdrop.module.scss'
 
 /**
  * Backdrop - stateless presentational component
  * @param {object} props - props
- * @param {string|array} props.className - list of class names, default: backdrop
  * @param {function} props.onClick - click handler
  * @return {object} An object of children element
  */
-const Backdrop = ({ className, onClick }) => (
-  <div className={cssClass(className)} onClick={onClick} />
+const Backdrop = ({ onClick }) => (
+  <div className={styles.backdrop} onClick={onClick} />
 )
 
 Backdrop.displayName = 'Backdrop'
 
 Backdrop.propTypes = {
-  className: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
-  /**
-   * Gets called when the user clicks on backdrop
-   *
-   * @param {SyntheticEvent} event The react `SyntheticEvent`
-   * @param {Object} All props
-   */
   onClick: PropTypes.func
 }
 
 Backdrop.defaultProps = {
-  className: 'backdrop',
   onClick: () => null
 }
 
