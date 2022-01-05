44bf5dd2ed4dd71602a9f47339cf182540bbfe50
Krzysztof Perzanowski <krzysztof.perzanowski@landingi.com>
2022-01-05 16:27:52 +0100

    [EDI-902]Divider css-module (#96)

--- src/shared/components/ui/Divider/Divider.test.js
+++ src/shared/components/ui/Divider/Divider.test.js
@@ -20,6 +20,41 @@ describe('<Divider/> mount', () => {
   })
 
   it('has `divider` class', () => {
-    expect(wrapper.hasClass('divider')).toBe(true)
+    expect(wrapper.find('div').hasClass('divider')).toBe(true)
+  })
+
+  it('has dropdown variant', () => {
+    wrapper.setProps({
+      variant: 'dropdown'
+    })
+    expect(wrapper.find('div').hasClass('divider--dropdown')).toBe(true)
+  })
+
+  it('has horizontal variant', () => {
+    wrapper.setProps({
+      variant: 'horizontal'
+    })
+    expect(wrapper.find('div').hasClass('divider--horizontal')).toBe(true)
+  })
+
+  it('has menu variant', () => {
+    wrapper.setProps({
+      variant: 'menu'
+    })
+    expect(wrapper.find('div').hasClass('divider--menu')).toBe(true)
+  })
+
+  it('has normal variant', () => {
+    wrapper.setProps({
+      variant: 'normal'
+    })
+    expect(wrapper.find('div').hasClass('divider--normal')).toBe(true)
+  })
+
+  it('has vertical align', () => {
+    wrapper.setProps({
+      align: 'vertical'
+    })
+    expect(wrapper.find('div').hasClass('divider--normal')).toBe(true)
   })
 })
