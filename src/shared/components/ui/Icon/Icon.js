efed168fcf52854135949d63d7724fa319f9695c
Krzysztof Perzanowski <k.pezanovski@gmail.com>
2022-01-10 16:23:41 +0100

    [EDI-907]

--- src/shared/components/ui/Icon/Icon.js
+++ src/shared/components/ui/Icon/Icon.js
@@ -1,30 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { styles } from '@helpers/css'
 import css from './fonts/css/editor-icons.css'
-import scss from './Icon.scss'
+import { styles } from '@helpers/css'
+import stylesScss from './Icon.module.scss'
+import { useStyles } from '@helpers/hooks/useStyles'
 
-const cssClass = styles({ ...css, ...scss })
-
+const cssClass = styles({ ...css })
 /**
  * Icon - stateless presentational component
  * @param {object} props - props
  * @param {string} props.icon - icon name
+ * @param {string|array} props.className - list of class names
  * @param {string} props.color - color of icon default, primary
  * @return {object} An object of children element
  */
-const Icon = ({ icon, color }) => (
-  <i className={cssClass('editor-icon', icon, `icon--${color}`)} />
-)
+const Icon = ({ icon, className, color }) => {
+  const elementStyles = useStyles({
+    [className]: true,
+    [stylesScss[`icon--${color}`]]: color,
+    [cssClass(icon)]: icon
+  })
+
+  console.log('css', cssClass(icon))
+
+  return <i className={elementStyles} />
+}
 
 Icon.displayName = 'Icon'
 
 Icon.propTypes = {
   icon: PropTypes.string.isRequired,
+  className: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
   color: PropTypes.string
 }
 
 Icon.defaultProps = {
+  className: 'editor-icon',
   color: 'default'
 }
 
