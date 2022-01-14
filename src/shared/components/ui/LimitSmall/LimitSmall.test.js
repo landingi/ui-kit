e46d6fe8d523b8f5a438743e569a248bf88f8c00
Jarosław Kucharski <72187174+jaroslawkucharski-landingi@users.noreply.github.com>
2022-01-14 09:21:55 +0100

    Test fixed

--- src/shared/components/ui/LimitSmall/LimitSmall.test.js
+++ src/shared/components/ui/LimitSmall/LimitSmall.test.js
@@ -2,20 +2,18 @@ import React from 'react'
 import { mountWithIntl } from '@jestutils'
 import LimitSmall from '@components/ui/LimitSmall'
 
-const limitSmallComponent = (
-  <LimitSmall
-    limit={20000}
-    limitText='word.unique.visitors'
-    padding='none'
-    quantity={5}
-  />
-)
-
-describe('<Limit /> mount', () => {
+describe('<LimitSmall /> mount', () => {
   let wrapper
 
+  const props = {
+    limit: 20000,
+    limitText: 'word.unique.visitors',
+    padding: 'none',
+    quantity: 5
+  }
+
   beforeEach(() => {
-    wrapper = mountWithIntl(limitSmallComponent)
+    wrapper = mountWithIntl(<LimitSmall {...props} />)
   })
 
   afterEach(() => {
@@ -27,14 +25,76 @@ describe('<Limit /> mount', () => {
   })
 
   it('has none padding', () => {
-    expect(wrapper.find('.result__dropdown').hasClass('padding__none')).toBe(
-      true
-    )
+    expect(
+      wrapper.find('.result__dropdown .padding__none').hasClass('padding__none')
+    ).toBe(true)
   })
 
   it('quantity should be `word.unique.visitors 5 / 20 000`', () => {
-    expect(wrapper.find('.result__dropdown').text()).toEqual(
+    expect(wrapper.find('.result__dropdown').at(1).text()).toEqual(
       'word.unique.visitors5 / 20 000'
     )
+  })
+})
+
+describe('<LimitSmall /> mount', () => {
+  let wrapper
+
+  const props = {
+    limit: 20000,
+    limitText: 'word.unique.visitors',
+    padding: 'small',
+    quantity: 5
+  }
+
+  beforeEach(() => {
+    wrapper = mountWithIntl(<LimitSmall {...props} />)
+  })
+
+  afterEach(() => {
+    wrapper.unmount()
+  })
+
+  it('is mounted', () => {
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('has small padding', () => {
+    expect(
+      wrapper
+        .find('.result__dropdown .padding__small')
+        .hasClass('padding__small')
+    ).toBe(true)
+  })
+})
+
+describe('<LimitSmall /> mount', () => {
+  let wrapper
+
+  const props = {
+    limit: 20000,
+    limitText: 'word.unique.visitors',
+    padding: 'medium',
+    quantity: 5
+  }
+
+  beforeEach(() => {
+    wrapper = mountWithIntl(<LimitSmall {...props} />)
+  })
+
+  afterEach(() => {
+    wrapper.unmount()
+  })
+
+  it('is mounted', () => {
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('has medium padding', () => {
+    expect(
+      wrapper
+        .find('.result__dropdown .padding__medium')
+        .hasClass('padding__medium')
+    ).toBe(true)
   })
 })
