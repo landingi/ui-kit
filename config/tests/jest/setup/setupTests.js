3ba762c830cfcf510accae7062fa83eb7ddad4d9
Lukasz Ochwat <lukasz.ochwat@landingi.com>
2022-01-14 11:26:18 +0100

    remove library globally

--- config/tests/jest/setup/setupTests.js
+++ config/tests/jest/setup/setupTests.js
@@ -2,6 +2,5 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 import { configure } from 'enzyme'
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
-import '@testing-library/jest-dom'
 
 configure({ adapter: new Adapter() })
