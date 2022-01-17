26f1fd7e17e537ff098dcff64e61a6d36dfa1da0
Krzysztof Perzanowski <k.pezanovski@gmail.com>
2022-01-17 22:50:16 +0100

    [EDI-926]Overflow css module

--- src/shared/components/ui/Overflow/Overflow.test.js
+++ src/shared/components/ui/Overflow/Overflow.test.js
@@ -1,29 +1,12 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { render } from '@jestutils'
 import Overflow from '@components/ui/Overflow'
 
 const props = {
   children: 'placeholder'
 }
-
-const overflowComponent = <Overflow {...props}>{props.children}</Overflow>
-
 describe('<Overflow/> mount', () => {
-  let wrapper
-
-  beforeEach(() => {
-    wrapper = mount(overflowComponent)
-  })
-
-  afterEach(() => {
-    wrapper.unmount()
-  })
-
   it('is mounted', () => {
-    expect(wrapper.exists()).toBe(true)
-  })
-
-  it('has `overflow` class', () => {
-    expect(wrapper.hasClass('overflow')).toBe(true)
+    render(<Overflow {...props}>{props.children}</Overflow>)
   })
 })
