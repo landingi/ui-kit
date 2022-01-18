1f3cbb56f242ba1021c0735251235abeb41b9c0d
Krzysztof Perzanowski <krzysztof.perzanowski@landingi.com>
2022-01-18 18:45:04 +0100

    [EDI-928]Panel css module (#136)

--- src/shared/components/ui/Panel/Panel.test.js
+++ src/shared/components/ui/Panel/Panel.test.js
@@ -1,29 +1,13 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { render } from '@jestutils'
 import Panel from '@components/ui/Panel'
 
 const props = {
   children: 'placeholder'
 }
 
-const PanelComponent = <Panel {...props}>{props.children}</Panel>
-
 describe('<Panel/> mount', () => {
-  let wrapper
-
-  beforeEach(() => {
-    wrapper = mount(PanelComponent)
-  })
-
-  afterEach(() => {
-    wrapper.unmount()
-  })
-
   it('is mounted', () => {
-    expect(wrapper.exists()).toBe(true)
-  })
-
-  it('has `panel` class', () => {
-    expect(wrapper.find('div').hasClass('panel')).toBe(true)
+    render(<Panel {...props}>{props.children}</Panel>)
   })
 })
