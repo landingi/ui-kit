86b7b86a2861a5d6e7c42820761bddb1140fbba1
Jarosław Kucharski <72187174+jaroslawkucharski-landingi@users.noreply.github.com>
2022-01-21 13:40:59 +0100

    CR fixed

--- src/shared/components/ui/Paragraph/Paragraph.test.js
+++ src/shared/components/ui/Paragraph/Paragraph.test.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { render } from '@jestutils'
-import registerIcons from '@helpers/icons'
 import Paragraph from '@components/ui/Paragraph'
-
-registerIcons()
 
 describe('<Paragraph /> mount', () => {
   it('is mounted', () => {
