29f30306be4f3acb17f0175c6d3050f9b30580c4
Bartłomiej Kochanowicz <bartlomiej.kochanowicz@landingi.com>
2022-01-25 11:42:30 +0100

    backdrop z-index

--- src/shared/components/ui/Backdrop/Backdrop.js
+++ src/shared/components/ui/Backdrop/Backdrop.js
@@ -10,10 +10,12 @@ import styles from './Backdrop.module.scss'
  * @param {function} props.onClick - click handler
  * @return {object} An object of children element
  */
-const Backdrop = ({ className, onClick }) => {
+const Backdrop = ({ className, onClick, zIndex }) => {
   const backdropStyles = useStyles(
     {
-      [styles.backdrop]: true
+      [styles.backdrop]: true,
+      [styles['backdrop__index-4']]: String(zIndex) === '4',
+      [styles['backdrop__index-6']]: String(zIndex) === '6'
     },
     className
   )
@@ -25,12 +27,14 @@ Backdrop.displayName = 'Backdrop'
 
 Backdrop.propTypes = {
   className: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
-  onClick: PropTypes.func
+  onClick: PropTypes.func,
+  zIndex: PropTypes.oneOf(['4', '6'])
 }
 
 Backdrop.defaultProps = {
   onClick: () => null,
-  className: ''
+  className: '',
+  zIndex: null
 }
 
 export default Backdrop
