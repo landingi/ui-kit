d3ca829480bd8dbcbe610566cbcf9158c7c0236d
Krzysztof Perzanowski <k.pezanovski@gmail.com>
2022-01-28 21:29:52 +0100

    Input className fix and TODO info

--- src/shared/components/ui/Input/Input.test.js
+++ src/shared/components/ui/Input/Input.test.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import Input from '@components/ui/Input'
-import registerIcons from '@helpers/icons'
-
-registerIcons()
 
 const mockedOnChange = jest.fn()
 const mockedOnKeyDown = jest.fn()
