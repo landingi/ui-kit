20c9d408d210047c2c26cb566235436ec15cd695
Oskar Jankowiak <oskar.jankowiak@landingi.com>
2022-02-16 13:40:51 +0100

    created tests

--- src/shared/helpers/position.test.js
+++ src/shared/helpers/position.test.js
@@ -1,4 +1,4 @@
-import { centerParent } from '@helpers/position'
+import { centerParent, isInViewVertical } from '@helpers/position'
 
 describe('Helpers centerParent', () => {
   it('should return a number', () => {
@@ -7,5 +7,25 @@ describe('Helpers centerParent', () => {
 
   it('isNaN return 0', () => {
     expect(centerParent(NaN, '1', '2')).toEqual(0)
+  })
+})
+
+describe('Helper isInViewVertical', () => {
+  it('Should return true if element is in view vertical', () => {
+    global.document.clientHeight = 1200
+
+    const div = document.createElement('div')
+
+    document.documentElement.clientHeight = 1200
+
+    div.getBoundingClientRect = jest.fn(() => ({
+      height: 300,
+      top: 1000,
+      bottom: 200
+    }))
+
+    // zamockować clientHeight dla document.documentelement w teście
+
+    // expect(isInViewVertical(div)).toBe(true)
   })
 })
