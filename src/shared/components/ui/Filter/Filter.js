4d830c3f9344014489c5c1ea990c7e49442f81b4
bartlomiej-kochanowicz <89905016+bartlomiej-kochanowicz@users.noreply.github.com>
2022-04-21 14:50:36 +0200

    [SPA-179]Add test-id to label and layout components (#233)

--- src/shared/components/ui/Filter/Filter.js
+++ src/shared/components/ui/Filter/Filter.js
@@ -15,9 +15,17 @@ import uuid from 'react-uuid'
  * @param {function} props.setValue - parent component filter setter
  * @param {string | object} props.initialValue - initial value
  * @param {string} props.localStorageKey - local storage key, if filter value should be remebered between sessions
+ * @param {string} props.customLabel - customLabel custom label component
  * @return {object} An object of children element
  */
-const Filter = ({ values, setValue, initialValue, localStorageKey }) => {
+const Filter = ({
+  values,
+  setValue,
+  initialValue,
+  localStorageKey,
+  customLabel,
+  ['data-testid']: dataTestId
+}) => {
   const findInitialValue = () => {
     const find = values.find(({ value }) => value === initialValue)
 
@@ -33,8 +41,10 @@ const Filter = ({ values, setValue, initialValue, localStorageKey }) => {
     emitCloseDropdown()
   }
 
+  const dropdownLabel = customLabel ? customLabel(filterLabel) : filterLabel
+
   return (
-    <Dropdown label={filterLabel} size='medium'>
+    <Dropdown label={dropdownLabel} size='medium' data-testid={dataTestId}>
       <List>
         {values.map(({ value, label }) => (
           <ListItem key={uuid()} variant='dropdown'>
@@ -66,13 +76,17 @@ Filter.propTypes = {
       ]),
       value: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
     })
-  ).isRequired
+  ).isRequired,
+  customLabel: PropTypes.func,
+  ['data-testid']: PropTypes.string
 }
 
 Filter.defaultProps = {
   initialValue: null,
   localStorageKey: null,
-  setValue: () => null
+  setValue: () => null,
+  customLabel: null,
+  ['data-testid']: 'filter-component'
 }
 
 export default Filter
--- src/shared/components/ui/Layout/Layout.js
+++ src/shared/components/ui/Layout/Layout.js
@@ -10,9 +10,15 @@ import { useStyles } from '@helpers/hooks/useStyles'
  * @param {string|array} props.className - list of class names, default: layout
  * @param {object} props.children - children
  * @param {string} props.width - width
+ * @param {string} props.datatestid - data-testid
  * @return {object} An object of children element
  */
-const Layout = ({ className, children, width }) => {
+const Layout = ({
+  className,
+  children,
+  width,
+  ['data-testid']: dataTestId
+}) => {
   const layoutStyles = useStyles(
     {
       [styles.layout]: true,
@@ -21,7 +27,11 @@ const Layout = ({ className, children, width }) => {
     className
   )
 
-  return <div className={layoutStyles}>{children}</div>
+  return (
+    <div className={layoutStyles} data-testid={dataTestId}>
+      {children}
+    </div>
+  )
 }
 
 Layout.displayName = 'Layout'
@@ -29,12 +39,14 @@ Layout.displayName = 'Layout'
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
-  width: PropTypes.oneOf(['large', 'big', 'full'])
+  width: PropTypes.oneOf(['large', 'big', 'full']),
+  ['data-testid']: PropTypes.string
 }
 
 Layout.defaultProps = {
   className: '',
-  width: 'full'
+  width: 'full',
+  ['data-testid']: 'layout-component'
 }
 
 export default Layout
