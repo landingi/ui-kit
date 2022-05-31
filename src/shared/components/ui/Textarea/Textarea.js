719cb718d2c14cceb5e9321932305f1eb51862e0
Oskar Jankowiak <89905001+OskarJankowiak-Landingi@users.noreply.github.com>
2022-05-31 09:48:55 +0200

    [EDI-1492]Updated textarea - css modules, props (#258)
    Css modules
    Added missing jsdoc described props
    Added custom className attribute to be able ovveride default styles

--- src/shared/components/ui/Textarea/Textarea.js
+++ src/shared/components/ui/Textarea/Textarea.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { styles } from '@helpers/css'
-import scss from './Textarea.scss'
+import styles from './Textarea.module.scss'
+import { useStyles } from '@helpers/hooks/useStyles'
 
-const cssClass = styles(scss)
+//TODO Textarea mdx, test
 
-//TODO Textarea css, mdx, test
 /**
  * Textarea - stateless presentational component
- * @param {object} i18n- props with translated string
+ * @param {string | array} className - list of custom css class from out of component
+ * @param {string} name - name
+ * @param {string} id - id
+ * @param {string} value - value
+ * @param {func} onChange - on change callback
+ * @param {object} i18n - props with translated string
+ * @return {object} An object of children element
  */
-const Textarea = ({ name, value, onChange, i18n }) => {
+const Textarea = ({ className, name, id, value, onChange, i18n }) => {
+  const textAreaStyles = useStyles(
+    {
+      [styles['textarea']]: true
+    },
+    className
+  )
+
   return (
     <textarea
-      className={cssClass('textarea')}
+      className={textAreaStyles}
+      id={id}
       name={name}
       placeholder={i18n.placeholder}
       onChange={onChange}
@@ -25,12 +38,18 @@ const Textarea = ({ name, value, onChange, i18n }) => {
 Textarea.displayName = 'Textarea'
 
 Textarea.propTypes = {
+  className: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
   name: PropTypes.string,
+  id: PropTypes.string,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   i18n: PropTypes.shape({
     placeholder: PropTypes.string
   }).isRequired
+}
+
+Textarea.defaultProps = {
+  className: ''
 }
 
 export default Textarea
