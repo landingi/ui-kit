b8407744ca4a52942b791dac1c132be5b51737b5
Bartlomiej Kochanowicz <bartlomiej.kochanowicz@landingi.com>
2022-06-08 14:31:08 +0200

    build

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
 import postcss from 'rollup-plugin-postcss'
+import external from 'rollup-plugin-peer-deps-external'
+import { terser } from 'rollup-plugin-terser'
 
 const packageJson = require('./package.json')
 
@@ -22,13 +24,15 @@ export default [
     ],
     plugins: [
       babel(),
+      external(),
       resolve(),
       commonjs(),
       postcss({
         // extract: 'app-uikit.css',
         modules: true,
         use: [['sass', { data: '@import "src/styles/theme.scss";' }]]
-      })
+      }),
+      terser()
     ]
   }
 ]
