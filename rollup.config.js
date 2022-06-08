9bc2cc204b0911f063b6e08e83ad52f40a8fbfef
Bartlomiej Kochanowicz <bartlomiej.kochanowicz@landingi.com>
2022-06-08 14:12:05 +0200

    build

--- rollup.config.js
+++ rollup.config.js
@@ -4,17 +4,19 @@ import babel from '@rollup/plugin-babel'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import postcss from 'rollup-plugin-postcss'
 
+const packageJson = require('./package.json')
+
 export default [
   {
     input: 'src/index.js',
     output: [
-      /*       {
+      {
         file: packageJson.main,
         format: 'cjs',
         sourcemap: true
-      }, */
+      },
       {
-        file: 'dist/index.js',
+        file: packageJson.module,
         format: 'esm',
         sourcemap: true
       }
@@ -27,7 +29,7 @@ export default [
       resolve(),
       commonjs(),
       postcss({
-        extract: 'app-uikit.css',
+        // extract: 'app-uikit.css',
         modules: true,
         use: [['sass', { data: '@import "src/styles/theme.scss";' }]]
       })
