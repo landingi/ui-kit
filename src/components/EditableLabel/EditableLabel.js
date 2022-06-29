f73536c2870a2f76452373e0f2912eb1202c8d83
Jarosław Kucharski <72187174+jaroslawkucharski-landingi@users.noreply.github.com>
2022-06-29 12:04:05 +0200

    [APP-149]Created EditableLabel (#287)
    Created EditableLabel.

---
+++ src/components/EditableLabel/EditableLabel.js
@@ -0,0 +1,171 @@
+import React, { useState, useCallback, useRef } from 'react'
+import PropTypes from 'prop-types'
+import { Row } from 'simple-flexbox'
+import Button from '@components/Button'
+import Icon from '@components/Icon'
+import Spreader from '@components/Spreader'
+import OverflowTooltip from '@components/OverflowTooltip'
+import Spinner from '@components/Spinner'
+import { useHover } from '@helpers/hooks/useHover'
+import { useStyles } from '@helpers/hooks/useStyles'
+import { useDetectOutsideClick } from '@helpers/hooks/useDetectOutsideClick'
+import styles from './EditableLabel.module.scss'
+
+/**
+ * EditableLabel - statefull input component
+ * @param {object} props - props
+ * @param {string} props.initialName - init name
+ * @param {string} props.placeholder - input placeholder
+ * @param {string} props.size - label size
+ * @param {function} props.onChange - callback function
+ * @param {bool} props.isLoading - is loading
+ * @param {bool} props.isDisabled - is disabled
+ * @param {bool} props.isClickable - is clickable
+ * @return {object} An object of children element
+ */
+const EditableLabel = ({
+  initialName,
+  placeholder,
+  size,
+  onChange,
+  isLoading,
+  isDisabled,
+  isClickable
+}) => {
+  const [wrapperProps, isHoveredWrapper] = useHover()
+  const [isFocused, setFocused] = useState(false)
+
+  const [name, setName] = useState(initialName)
+  const labelRef = useRef(null)
+  const wrapperRef = useRef(null)
+
+  const handleChange = event => setName(event.target.value)
+
+  const wrapperStyles = useStyles({
+    [styles.wrapper]: true,
+    [styles[`wrapper--${size}`]]: size
+  })
+
+  const inputStyles = useStyles({
+    [styles.input]: true,
+    [styles[`input--${size}`]]: size
+  })
+
+  const labelStyles = useStyles({
+    [styles.label]: true,
+    [styles[`label--${size}`]]: size
+  })
+
+  const buttonStyles = useStyles({
+    [styles.button]: true,
+    [styles['button--active']]: isHoveredWrapper || isFocused
+  })
+
+  const barStyles = useStyles({
+    [styles['input-bar']]: true,
+    [styles['input-bar--focus']]: isFocused
+  })
+
+  const handleAccept = useCallback(() => {
+    if (name.length === 0 || name === initialName) {
+      setName(name)
+    } else {
+      onChange(name)
+    }
+
+    setFocused(false)
+  }, [name, setName, onChange, setFocused, initialName])
+
+  const handleKeyDown = useCallback(
+    event => {
+      if (event.key === 'Enter') {
+        handleAccept()
+      }
+    },
+    [handleAccept]
+  )
+
+  const handleFocus = useCallback(() => {
+    setFocused(true)
+  }, [setFocused])
+
+  const handeOutsideClick = useCallback(() => {
+    setFocused(false)
+    setName(initialName)
+  }, [setFocused, name])
+
+  useDetectOutsideClick(wrapperRef, handeOutsideClick)
+
+  return (
+    <div className={wrapperStyles} ref={wrapperRef} {...wrapperProps}>
+      <Row className={styles['input-wrapper']} vertical='center'>
+        {!isFocused ? (
+          <span
+            className={labelStyles}
+            ref={labelRef}
+            onClick={isClickable ? handleFocus : () => null}
+            data-testid='editable-label'
+          >
+            <OverflowTooltip
+              content={name}
+              length={size === 'small' ? 21 : 44}
+              placement='top'
+            />
+          </span>
+        ) : (
+          <input
+            className={inputStyles}
+            value={name}
+            placeholder={placeholder}
+            onChange={handleChange}
+            type='text'
+            onKeyDown={handleKeyDown}
+            data-testid='editable-label-input'
+          />
+        )}
+
+        <span className={barStyles} />
+      </Row>
+
+      <Spreader spread='tiny' />
+
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        !isDisabled && (
+          <Button
+            variant='icon-transparent'
+            className={buttonStyles}
+            onClick={isFocused ? handleAccept : handleFocus}
+            size={size === 'small' ? 'mini' : 'medium'}
+            data-testid='editable-label-button'
+          >
+            <Icon icon={isFocused ? 'icon-ok' : 'icon-create'} />
+          </Button>
+        )
+      )}
+    </div>
+  )
+}
+
+EditableLabel.displayName = 'EditableLabel'
+
+EditableLabel.propTypes = {
+  initialName: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  size: PropTypes.oneOf(['small', 'big']),
+  onChange: PropTypes.func,
+  isLoading: PropTypes.bool,
+  isDisabled: PropTypes.bool,
+  isClickable: PropTypes.bool
+}
+
+EditableLabel.defaultProps = {
+  placeholder: '',
+  size: 'big',
+  isLoading: false,
+  isDisabled: false,
+  isClickable: false
+}
+
+export default EditableLabel
---
+++ src/components/EditableLabel/EditableLabel.test.js
@@ -0,0 +1,163 @@
+import React from 'react'
+import { render, screen, fireEvent } from '@jestutils'
+import EditableLabel from './EditableLabel'
+import '@testing-library/jest-dom'
+
+describe('<EditableLabel /> tests', () => {
+  it('renders properly', () => {
+    render(<EditableLabel initialName='test name' />)
+  })
+
+  it('input focus on button click', () => {
+    render(<EditableLabel initialName='test name' />)
+
+    const button = screen.getByTestId('editable-label-button')
+
+    fireEvent.click(button)
+
+    const input = screen.getByTestId('editable-label-input')
+
+    expect(input).toBeInTheDocument()
+  })
+
+  it('the label should be clickable', () => {
+    render(<EditableLabel initialName='test name' isClickable />)
+
+    const label = screen.getByTestId('editable-label')
+
+    fireEvent.click(label)
+
+    const input = screen.getByTestId('editable-label-input')
+
+    expect(input).toBeInTheDocument()
+  })
+
+  it('input blur on outside click', () => {
+    render(<EditableLabel initialName='test name' />)
+
+    const button = screen.getByTestId('editable-label-button')
+
+    fireEvent.click(button)
+
+    const input = screen.getByTestId('editable-label-input')
+
+    fireEvent.mouseDown(document)
+    expect(input).not.toBeInTheDocument()
+  })
+
+  it('call callback function on accept by button', () => {
+    const mockOnChange = jest.fn()
+
+    render(<EditableLabel initialName='test name' onChange={mockOnChange} />)
+
+    const button = screen.getByTestId('editable-label-button')
+
+    //start edit
+    fireEvent.click(button)
+
+    const input = screen.getByTestId('editable-label-input')
+
+    //change name
+    fireEvent.change(input, { target: { value: 'new name' } })
+    //accept edit
+    fireEvent.click(button)
+
+    expect(mockOnChange).toBeCalledWith('new name')
+  })
+
+  it('call callback function on accept by enter', () => {
+    const mockOnChange = jest.fn()
+
+    render(<EditableLabel initialName='test name' onChange={mockOnChange} />)
+
+    const button = screen.getByTestId('editable-label-button')
+
+    //start edit
+    fireEvent.click(button)
+
+    const input = screen.getByTestId('editable-label-input')
+
+    //change name
+    fireEvent.change(input, { target: { value: 'new name' } })
+    //accept edit
+    fireEvent.keyDown(input, { key: 'Enter', code: 13 })
+
+    expect(mockOnChange).toBeCalledWith('new name')
+  })
+
+  it('cancel edit when new title is empty', () => {
+    const mockOnChange = jest.fn()
+
+    render(<EditableLabel initialName='test name' onChange={mockOnChange} />)
+
+    const button = screen.getByTestId('editable-label-button')
+
+    //start edit
+    fireEvent.click(button)
+
+    const input = screen.getByTestId('editable-label-input')
+
+    //change name
+    fireEvent.change(input, { target: { value: '' } })
+    //accept edit
+    fireEvent.click(button)
+
+    expect(mockOnChange).not.toBeCalled()
+  })
+
+  it('cancel edit when new title the same as the initial name', () => {
+    const mockOnChange = jest.fn()
+
+    render(<EditableLabel initialName='test name' onChange={mockOnChange} />)
+
+    const button = screen.getByTestId('editable-label-button')
+
+    //start edit
+    fireEvent.click(button)
+
+    const input = screen.getByTestId('editable-label-input')
+
+    //change name
+    fireEvent.change(input, { target: { value: 'test name' } })
+    //accept edit
+    fireEvent.click(button)
+
+    expect(mockOnChange).not.toBeCalled()
+  })
+
+  it('button should have a mini size', () => {
+    render(<EditableLabel initialName='test name' size='small' />)
+
+    const button = screen.getByTestId('editable-label-button')
+
+    expect(button).toHaveClass('button--mini')
+  })
+
+  it('button should have a medium size', () => {
+    render(<EditableLabel initialName='test name' size='big' />)
+
+    const button = screen.getByTestId('editable-label-button')
+
+    expect(button).toHaveClass('button--medium')
+  })
+
+  it('should have a spinner instead of a button', () => {
+    render(<EditableLabel initialName='test name' isLoading />)
+
+    const spinner = screen.getByTestId('spinner')
+
+    expect(spinner).toBeInTheDocument()
+  })
+
+  it('should have a spinner instead of a button', () => {
+    const { rerender } = render(<EditableLabel initialName='test name' />)
+
+    const button = screen.getByTestId('editable-label-button')
+
+    expect(button).toBeInTheDocument()
+
+    rerender(<EditableLabel initialName='test name' isDisabled />)
+
+    expect(button).not.toBeInTheDocument()
+  })
+})
