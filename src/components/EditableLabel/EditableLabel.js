65efc4cb19493606a022194222577d8a201dfe6b
Bartlomiej Kochanowicz <bartlomiej.kochanowicz@landingi.com>
2022-07-25 15:23:42 +0200

    del console log

--- src/components/EditableLabel/EditableLabel.js
+++ src/components/EditableLabel/EditableLabel.js
@@ -103,11 +103,6 @@ const EditableLabel = ({
 
   useDetectOutsideClick(containerRef, handeOutsideClick)
 
-  console.log({
-    scrollWidth: labelRef?.current?.scrollWidth,
-    offsetWidth: labelRef?.current?.offsetWidth
-  })
-
   return (
     <div className={containerStyles} ref={containerRef} {...wrapperProps}>
       <Row className={styles.wrapper} vertical='center'>
