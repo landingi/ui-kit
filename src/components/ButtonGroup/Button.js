9d40a2429e78ed709c4cba86bd174e7012ed5748
Jarosław Kucharski <72187174+jaroslawkucharski-landingi@users.noreply.github.com>
2022-09-21 14:37:48 +0200

    Added prop disabled to button group

--- src/components/ButtonGroup/Button.js
+++ src/components/ButtonGroup/Button.js
@@ -8,9 +8,10 @@ import styles from './ButtonGroup.module.scss'
  * Button - statefull presentational component
  * @param {node} props.children - children of the component
  * @param {string} props.id - id of the component
+ * @param {bool} props.isDisabled - disabled status, default: false
  * @return {object} An object of children element
  */
-const Button = ({ children, id }) => {
+const Button = ({ children, id, isDisabled }) => {
   const { selected, setSelected } = useButtonGroupContext()
 
   const buttonStyles = useStyles({
@@ -21,7 +22,11 @@ const Button = ({ children, id }) => {
   const handleChange = () => setSelected(id)
 
   return (
-    <button className={buttonStyles} onClick={handleChange}>
+    <button
+      className={buttonStyles}
+      onClick={handleChange}
+      disabled={isDisabled}
+    >
       {children}
     </button>
   )
@@ -31,7 +36,12 @@ Button.displayName = 'ButtonGroupButton'
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
-  id: PropTypes.string.isRequired
+  id: PropTypes.string.isRequired,
+  isDisabled: PropTypes.bool
+}
+
+Button.defaultProps = {
+  isDisabled: false
 }
 
 export default Button
