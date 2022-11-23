805a6594fc4a48f4388281640b80433292b1a0a1
Lukasz Ochwat <lukasz.ochwat@landingi.com>
2022-11-23 08:23:12 +0100

    add type to elementStyles

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -24,7 +24,7 @@ const Image: FC<ImageProps> = ({
   small = false,
   loadingAttr = 'eager'
 }) => {
-  const elementStyles = useStyles(
+  const elementStyles: string = useStyles(
     {
       [styles['image--auto']]: auto,
       [styles['image--small']]: small
