cdef8f913a78683109583877262417ff0ffca9d1
Oskar Jankowiak <oskar.jankowiak@landingi.com>
2022-11-30 09:21:33 +0100

    build

---
+++ dist/components/ProgressBar/ProgressBar.d.ts
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+export interface ProgressBarProps {
+    size?: 'small' | 'medium';
+    variant: 'success' | 'warning' | 'alert' | 'progress' | 'brand';
+    quantity: number;
+    limit?: number;
+    border?: 'white' | 'grey';
+    i18n?: {
+        limitAlert: string;
+    };
+    showValue?: boolean;
+    valueSize?: 10 | 12 | 16 | 18 | 32 | 44 | 62;
+    withoutAnimation?: boolean;
+}
+declare const ProgressBar: FC<ProgressBarProps>;
+export default ProgressBar;
