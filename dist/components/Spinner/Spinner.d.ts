268db689ece6390368eb525b57d9509ed920d2da
Oskar Jankowiak <oskar.jankowiak@landingi.com>
2022-11-30 11:23:37 +0100

    build

---
+++ dist/components/Spinner/Spinner.d.ts
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+interface SpinnerProps {
+    className?: string | string[];
+}
+declare const Spinner: FC<SpinnerProps>;
+export default Spinner;
---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export * from './components';
+export { emitCloseDropdown } from './events/dropdown';
+export { emitTimingToastToggle, emitToastToggle } from './events/toast';
