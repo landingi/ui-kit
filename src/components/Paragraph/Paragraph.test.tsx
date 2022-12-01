3536ac51b00a486131292a5b1e77271eeae4900b
Jarosław Kucharski <72187174+jaroslawkucharski-landingi@users.noreply.github.com>
2022-12-01 13:10:19 +0100

    [LAND-139]Rewriting the Paragraph component to TypeScript (#386)
    The Paragraph component was rewritten to TypeScript.

--- src/components/Paragraph/Paragraph.test.tsx
+++ src/components/Paragraph/Paragraph.test.tsx
@@ -1,6 +1,5 @@
-import Paragraph from '@components/Paragraph'
+import { Paragraph } from '@components/Paragraph'
 import { render } from '@testing-library/react'
-import React from 'react'
 
 describe('<Paragraph /> mount', () => {
   it('is mounted', () => {
---
+++ src/components/Paragraph/Paragraph.tsx
@@ -0,0 +1,57 @@
+import { useStyles } from '@helpers/hooks/useStyles'
+import { FC, ReactNode } from 'react'
+
+import styles from './Paragraph.module.scss'
+
+interface ParagraphProps {
+  className?: string | string[]
+  children: ReactNode
+  color?:
+    | 'accent-1'
+    | 'accent-2'
+    | 'accent-3'
+    | 'accent-4'
+    | 'accent-5'
+    | 'accent-6'
+    | 'accent-7'
+    | 'info'
+    | 'white'
+    | 'color-8'
+    | 'success'
+  size?: 10 | 12 | 14 | 16 | 18
+  align?: 'left' | 'center' | 'right'
+  padding?: 'small' | 'medium' | 'none'
+  weight?: 300 | 400 | 700
+  uppercase?: boolean
+  line?: 18 | 20 | 22 | 24 | 28
+}
+
+export const Paragraph: FC<ParagraphProps> = ({
+  className = '',
+  children,
+  color = 'accent-1',
+  size = 14,
+  align = 'left',
+  padding = 'medium',
+  weight = 300,
+  uppercase = false,
+  line = 18
+}) => {
+  const paragraphStyles = useStyles(
+    {
+      [styles.paragraph]: true,
+      [styles[`paragraph-color--${color}`]]: color,
+      [styles[`paragraph-size--${size}`]]: size,
+      [styles[`paragraph-padding--${padding}`]]: padding,
+      [styles[`paragraph-align--${align}`]]: align,
+      [styles[`paragraph-weight--${weight}`]]: weight,
+      [styles['paragraph-uppercase']]: uppercase,
+      [styles[`paragraph-line--${line}`]]: line
+    },
+    className
+  )
+
+  return <p className={paragraphStyles}>{children}</p>
+}
+
+Paragraph.displayName = 'Paragraph'
