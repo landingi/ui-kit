34bdd87ff216d254582e32469de22f160c4c0cef
Jarosław Kucharski <72187174+jaroslawkucharski-landingi@users.noreply.github.com>
2022-12-01 13:03:25 +0100

    lint fixed

--- src/components/Paragraph/Paragraph.test.tsx
+++ src/components/Paragraph/Paragraph.test.tsx
@@ -1,7 +1,6 @@
 import { Paragraph } from '@components/Paragraph'
 import { render } from '@testing-library/react'
 
-
 describe('<Paragraph /> mount', () => {
   it('is mounted', () => {
     render(<Paragraph>Test</Paragraph>)
