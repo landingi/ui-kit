0f74d7b215ddc0ea82943abe8fc881aa83eabcc2
Oskar Jankowiak <oskar.jankowiak@landingi.com>
2022-12-01 09:35:29 +0100

    build

--- dist/components/InfoBar/InfoBar.d.ts
+++ dist/components/InfoBar/InfoBar.d.ts
@@ -2,7 +2,7 @@ import { FC, ReactNode } from 'react';
 export interface InfoBarProps {
     children: ReactNode;
     className?: string | string[];
-    type?: 'warning' | 'info' | 'alert' | 'success';
+    type?: 'warning' | 'info' | 'alert';
 }
 declare const InfoBar: FC<InfoBarProps>;
 export default InfoBar;
