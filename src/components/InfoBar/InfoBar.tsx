a79df96ba071e64703e7b999d3ec76df9c845462
Oskar Jankowiak <oskar.jankowiak@landingi.com>
2022-12-01 09:12:30 +0100

    rewritted InfoBar from js to ts

---
+++ src/components/InfoBar/InfoBar.tsx
@@ -0,0 +1,51 @@
+import Icon from '@components/Icon'
+import { useStyles } from '@helpers/hooks/useStyles'
+import { FC, ReactNode } from 'react'
+
+import styles from './InfoBar.module.scss'
+
+export interface InfoBarProps {
+  children: ReactNode
+  className?: string | string[]
+  type?: 'warning' | 'info' | 'alert' | 'success'
+}
+
+const InfoBar: FC<InfoBarProps> = ({
+  children,
+  className = '',
+  type = 'info'
+}) => {
+  const elementClasses = useStyles(
+    {
+      [styles[`info-bar`]]: true,
+      [styles[`info-bar--${type}`]]: type
+    },
+    className
+  )
+
+  const boxClasses = useStyles({
+    [styles[`info-bar--box`]]: true,
+    [styles[`info-bar--${type}-box`]]: type
+  })
+
+  const icon = {
+    warning: 'exclamation',
+    alert: 'exclamation-triangle',
+    info: 'info',
+    success: 'info'
+  }[type]
+
+  return (
+    <div data-testid='infobar-wrapper' className={elementClasses}>
+      <div className={boxClasses}>
+        <Icon icon={icon} />
+      </div>
+
+      <div className={styles.info__bar}>{children}</div>
+    </div>
+  )
+}
+
+InfoBar.displayName = 'InfoBar'
+
+export default InfoBar
