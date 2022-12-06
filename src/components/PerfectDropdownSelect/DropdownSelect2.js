a624e4e072e25fadb5a23ce52d5dee4d5004b77f
Lukasz Ochwat <lukasz.ochwat@landingi.com>
2022-12-06 08:51:51 +0100

    linter fix

--- src/components/PerfectDropdownSelect/DropdownSelect2.js
+++ src/components/PerfectDropdownSelect/DropdownSelect2.js
@@ -162,7 +162,9 @@ const PerfectDropdownSelect = ({
   const [searchValue, setSearchValue] = useState(null)
 
   const handleSearchOptionsChange = value =>
-    handleOnSearchChange() === null? setSearchValue(value): handleOnSearchChange(value)
+    handleOnSearchChange() === null
+      ? setSearchValue(value)
+      : handleOnSearchChange(value)
 
   /**
    * Clear value in search / searcher
