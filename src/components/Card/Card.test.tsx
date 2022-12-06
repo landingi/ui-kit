4bff87e2d0cb606461aabd4a3bf1d34acdf0c58a
Jarosław Kucharski <72187174+jaroslawkucharski-landingi@users.noreply.github.com>
2022-12-06 08:31:05 +0100

    Card rewritten to TS

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,18 +1,16 @@
 import '@testing-library/jest-dom'
 
-import Card from '@components/Card'
+import { Card } from '@components/Card'
 import { Paragraph } from '@components/Paragraph'
 import { render } from '@testing-library/react'
-import React from 'react'
 
 describe('<Card /> mount', () => {
-  const props = {
-    variant: 'alert',
-    children: <Paragraph>Alert text</Paragraph>
-  }
-
   it('should display proper text', () => {
-    const { getByText } = render(<Card {...props} />)
+    const { getByText } = render(
+      <Card variant='alert'>
+        <Paragraph>Alert text</Paragraph>
+      </Card>
+    )
 
     const paragraph = getByText('Alert text')
 
@@ -20,7 +18,11 @@ describe('<Card /> mount', () => {
   })
 
   it('should display alert variant', () => {
-    const { getByTestId } = render(<Card {...props} />)
+    const { getByTestId } = render(
+      <Card variant='alert'>
+        <Paragraph>Alert text</Paragraph>
+      </Card>
+    )
 
     const card = getByTestId('card')
 
