4aea5af4afbbf19c588c9d043d07e1b2c8b0290f
Jarosław Kucharski <72187174+jaroslawkucharski-landingi@users.noreply.github.com>
2022-12-13 14:17:20 +0100

    [LAND-127][LAND-128][LAND-131]Rewriting the LimitSmall, LimitQuantity & Logo components to TypeScript (#406)
    The LimitSmall, LimitQuantity & Logo components were rewritten to TypeScript.

---
+++ src/components/LimitSmall/LimitSmall.tsx
@@ -0,0 +1,43 @@
+import { formatNumeric } from '@helpers/data'
+import { useStyles } from '@helpers/hooks/useStyles'
+import { FC } from 'react'
+
+import styles from './LimitSmall.module.scss'
+
+interface LimitSmallProps {
+  className?: string | string[]
+  padding?: 'none' | 'tiny' | 'small' | 'medium'
+  limit: number
+  quantity: number
+  limitText: string
+}
+
+export const LimitSmall: FC<LimitSmallProps> = ({
+  className = '',
+  padding = 'none',
+  limit,
+  quantity,
+  limitText
+}) => {
+  const limitSmallStyles = useStyles(
+    {
+      [styles.result__dropdown]: true,
+      [styles[`padding__${padding}`]]: padding
+    },
+    className
+  )
+
+  return (
+    <div className={limitSmallStyles}>
+      <span>{limitText}</span>
+
+      <span>
+        <b>{formatNumeric(quantity)}</b>
+
+        {limit === -1 ? <span> / &#8734;</span> : ` / ${formatNumeric(limit)}`}
+      </span>
+    </div>
+  )
+}
+
+LimitSmall.displayName = 'LimitSmall'
