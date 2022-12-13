10598bfe2cf7f1e35231c87589a2e5f66e257747
Bartlomiej Kochanowicz <bartlomiej.kochanowicz@landingi.com>
2022-12-13 13:47:57 +0100

    absolute styles fix

--- src/components/Table/components/Body/BodyTr.tsx
+++ src/components/Table/components/Body/BodyTr.tsx
@@ -17,26 +17,28 @@ export const BodyTr = <Item extends ItemBase>({
   const [hoverProps, isHover] = useHover()
 
   return (
-    <tr className={styles.tr} {...hoverProps}>
-      {rowActions && !isHover && (
-        <div className={styles['tr__row-actions']}>{rowActions(item)}</div>
-      )}
+    <tr {...hoverProps}>
+      <div className={styles.tr}>
+        {rowActions && isHover && (
+          <div className={styles['tr__row-actions']}>{rowActions(item)}</div>
+        )}
 
-      {columns.map(column => {
-        if ((column as ColumnAccessor<Item>).accessor) {
+        {columns.map(column => {
+          if ((column as ColumnAccessor<Item>).accessor) {
+            return (
+              <td className={styles.td} key={column.identifier}>
+                {item[(column as ColumnAccessor<Item>).accessor] as ReactNode}
+              </td>
+            )
+          }
+
           return (
             <td className={styles.td} key={column.identifier}>
-              {item[(column as ColumnAccessor<Item>).accessor] as ReactNode}
+              {(column as CustomColumn<Item>).render(item)}
             </td>
           )
-        }
-
-        return (
-          <td className={styles.td} key={column.identifier}>
-            {(column as CustomColumn<Item>).render(item)}
-          </td>
-        )
-      })}
+        })}
+      </div>
     </tr>
   )
 }
