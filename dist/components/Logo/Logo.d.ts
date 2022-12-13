61835c1c4fdf55ab2b0f20f6d303b8bf60ac5f64
Jarosław Kucharski <72187174+jaroslawkucharski-landingi@users.noreply.github.com>
2022-12-13 13:58:55 +0100

    build

---
+++ dist/components/Logo/Logo.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+export declare const LongLogo: {
+    (): JSX.Element;
+    displayName: string;
+};
+export declare const ShortLogo: {
+    (): JSX.Element;
+    displayName: string;
+};
