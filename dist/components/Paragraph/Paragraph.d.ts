12fd84c046be3420f5ee844a7a09c85518b7c9c8
lukaszochwat-landingi <67641152+lukaszochwat-landingi@users.noreply.github.com>
2022-12-20 11:27:44 +0100

    [GRO-3443]Add justify align for Paragraph (#412)
    Add justify align for Paragraph.

--- dist/components/Paragraph/Paragraph.d.ts
+++ dist/components/Paragraph/Paragraph.d.ts
@@ -4,7 +4,7 @@ interface ParagraphProps {
     children: ReactNode;
     color?: 'accent-1' | 'accent-2' | 'accent-3' | 'accent-4' | 'accent-5' | 'accent-6' | 'accent-7' | 'info' | 'white' | 'color-8' | 'success';
     size?: 10 | 12 | 14 | 16 | 18;
-    align?: 'left' | 'center' | 'right';
+    align?: 'left' | 'center' | 'right' | 'justify';
     padding?: 'small' | 'medium' | 'none';
     weight?: 300 | 400 | 700;
     uppercase?: boolean;
