4fcf1561305122ca0a0dc1fe3200c7d32eedbd05
Bartlomiej Kochanowicz <bartlomiej.kochanowicz@landingi.com>
2023-01-04 14:00:19 +0100

    build

--- dist/components/Table/useTable.d.ts
+++ dist/components/Table/useTable.d.ts
@@ -2,6 +2,7 @@
 import { ItemBase, UseTableProps } from './types';
 export declare const useTable: <Item extends ItemBase>({ data, options, pagination, name, constantPageLimit, ...rest }: UseTableProps<Item>) => {
     Table: () => JSX.Element;
+    setPageIndex: import("react").Dispatch<import("react").SetStateAction<number>>;
     pageIndex: number;
     pageLimit: number;
     refresh: boolean;
