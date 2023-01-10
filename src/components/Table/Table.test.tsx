ca3b4087ab05ff0584cdfaeb82c8aa3137765d58
Bartlomiej Kochanowicz <bartlomiej.kochanowicz@landingi.com>
2023-01-10 12:01:49 +0100

    add pagination tests

--- src/components/Table/Table.test.tsx
+++ src/components/Table/Table.test.tsx
@@ -25,6 +25,60 @@ const mockData: Item[] = [
   }
 ]
 
+const mockDataWithPagination: Item[] = [
+  ...mockData,
+  {
+    identifier: 'f9c6dba4-b048-4c8b-a32b-c2b71c44d592',
+    name: 'test-page-3',
+    url: 'https://test-page-3.com'
+  },
+  {
+    identifier: '7beb3962-3cc6-49b3-8fcb-2f868ec717c1',
+    name: 'test-page-4',
+    url: 'https://test-page-4.com'
+  },
+  {
+    identifier: '8a813890-e65f-4399-b4b6-99e6130ef6e3',
+    name: 'test-page-5',
+    url: 'https://test-page-5.com'
+  },
+  {
+    identifier: '7aa78f81-dc67-4378-92b8-80d22e00e2f9',
+    name: 'test-page-6',
+    url: 'https://test-page-6.com'
+  },
+  {
+    identifier: 'd3e69485-3ac3-480b-bf64-8d55c901a38b',
+    name: 'test-page-7',
+    url: 'https://test-page-7.com'
+  },
+  {
+    identifier: 'fed2041a-f374-4055-9c14-f4f270ba9501',
+    name: 'test-page-8',
+    url: 'https://test-page-8.com'
+  },
+  {
+    identifier: 'ea2ea90c-a06d-47cb-9e78-efb80c5ba3ca',
+    name: 'test-page-9',
+    url: 'https://test-page-9.com'
+  },
+  {
+    identifier: '1d48eb60-6df2-4ced-b7b1-b573c674040e',
+    name: 'test-page-10',
+    url: 'https://test-page-10.com'
+  },
+  {
+    identifier: '97da75fe-7355-4404-8a31-196b2702978d',
+    name: 'test-page-11',
+    url: 'https://test-page-11.com'
+  },
+  {
+    identifier: 'ec940f0e-a3e9-4a2d-b1e8-c174b49d452f',
+    name: 'test-page-12',
+    url: 'https://test-page-12.com'
+  }
+]
+
 const columns: Column<Item>[] = [
   {
     identifier: 'name',
@@ -231,6 +285,63 @@ describe('useTable hook tests', () => {
       screen.getByTestId('filters-and-selectors-in-header')
     ).toBeInTheDocument()
     expect(screen.getByText('filters-and-sorters')).toBeInTheDocument()
+  })
+
+  it('renders properly with pagination and handleChange func', () => {
+    const mockHandlePageChange = jest.fn()
+
+    const { result } = renderHook(() =>
+      useTable<Item>({
+        name: 'test-table',
+        i18n,
+        data: mockDataWithPagination,
+        columns,
+        constantPageLimit: 2, // 2 items per page
+        pagination: {
+          current: 1,
+          total: 12,
+          handlePageChange: mockHandlePageChange
+        }
+      })
+    )
+
+    const { Table } = result.current
+
+    render(<Table />)
+
+    expect(screen.getByTestId('pagination-component')).toBeInTheDocument()
+
+    act(() => {
+      fireEvent.click(screen.getByTestId('pagination-next'))
+    })
+
+    expect(mockHandlePageChange).toBeCalledWith(2)
+  })
+
+  it('renders properly with pagination without handleChange func', () => {
+    const { result } = renderHook(() =>
+      useTable<Item>({
+        name: 'test-table',
+        i18n,
+        data: mockDataWithPagination,
+        columns,
+        constantPageLimit: 2, // 2 items per page
+        pagination: {
+          current: 1,
+          total: 12
+        }
+      })
+    )
+
+    const { Table } = result.current
+
+    render(<Table />)
+
+    expect(screen.getByTestId('pagination-component')).toBeInTheDocument()
+
+    act(() => {
+      fireEvent.click(screen.getByTestId('pagination-next'))
+    })
   })
 
   // screen.debug(undefined, Infinity)
