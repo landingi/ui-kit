64054c9c0810fad45c0e402d0ed8df514d5b928f
Jarosław Kucharski <72187174+jaroslawkucharski-landingi@users.noreply.github.com>
2023-01-19 08:51:08 +0100

    [APP-1096]Rewrite TextOverflow to TypeScript (#428)
    TextOverflow has been rewritten to TypeScript.

---
+++ dist/components/TextOverflow/TextOverflow.d.ts
@@ -0,0 +1,7 @@
+import { FC, ReactNode } from 'react';
+interface TextOverflowProps {
+    className?: string | string[];
+    children: ReactNode;
+}
+export declare const TextOverflow: FC<TextOverflowProps>;
+export {};
--- src/components/TextOverflow/TextOverflow.test.tsx
+++ src/components/TextOverflow/TextOverflow.test.tsx
@@ -1,16 +1,15 @@
 import '@testing-library/jest-dom'
 
-import TextOverflow from '@components/TextOverflow'
+import { TextOverflow } from '@components/TextOverflow'
 import { render, screen } from '@testing-library/react'
-import React from 'react'
 
 describe('<TextOverflow/> mount', () => {
   it('is mounted', () => {
-    render(<TextOverflow />)
+    render(<TextOverflow>Text</TextOverflow>)
   })
 
   it('has `overflow` class', () => {
-    render(<TextOverflow />)
+    render(<TextOverflow>Text</TextOverflow>)
 
     const overflow = screen.getByTestId('overflow')
 
