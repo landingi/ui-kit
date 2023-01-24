7c996c019160311d06ea3230bc06b73b4eb33d64
Bartlomiej Kochanowicz <bartlomiej.kochanowicz@landingi.com>
2023-01-24 11:42:22 +0100

    build

---
+++ dist/components/Textarea/Textarea.d.ts
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+export interface TextareaProps {
+    className?: string | string[];
+    name: string;
+    id: string;
+    value: string;
+    onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+    onBlur?: (event: React.FocusEvent<HTMLTextAreaElement>) => void;
+    variant?: 'default' | 'codearea';
+    size?: 'tiny' | 'small' | 'medium';
+    i18n?: {
+        placeholder?: string;
+        label?: string;
+    };
+    hasResize?: boolean;
+    maxHeight?: number;
+    disabled?: boolean;
+    errors?: Record<string, string>;
+    touched?: Record<string, boolean>;
+}
+export declare const Textarea: FC<TextareaProps>;
