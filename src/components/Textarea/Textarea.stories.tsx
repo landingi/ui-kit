3f64819d65ecdaea98a19ec33c5d50b10fa50328
Bartek Kochanowicz <89905016+bartlomiej-kochanowicz-landingi@users.noreply.github.com>
2023-01-24 11:45:09 +0100

    [APP-1097]Textarea - typescript & stories (#434)

---
+++ src/components/Textarea/Textarea.stories.tsx
@@ -0,0 +1,31 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import { Textarea } from './Textarea'
+
+export default {
+  title: 'Textarea',
+  component: Textarea
+} as ComponentMeta<typeof Textarea>
+
+const Template: ComponentStory<typeof Textarea> = args => <Textarea {...args} />
+
+export const Default = Template.bind({})
+
+Default.args = {
+  i18n: { placeholder: 'Textarea placeholder' },
+  name: 'textarea',
+  id: 'textarea',
+  onChange: () => {}
+}
+
+export const MaxHeightAndResize = Template.bind({})
+
+MaxHeightAndResize.args = {
+  i18n: { placeholder: 'Textarea placeholder' },
+  name: 'textarea',
+  id: 'textarea',
+  onChange: () => {},
+  size: 'tiny',
+  hasResize: true,
+  maxHeight: 180
+}
