5d9daf734d55ac934c2712a8ab16efd54ac83f87
Bartlomiej Kochanowicz <bartlomiej.kochanowicz@landingi.com>
2023-01-24 14:48:21 +0100

    build

--- dist/components/StatusIcon/StatusIcon.d.ts
+++ dist/components/StatusIcon/StatusIcon.d.ts
@@ -1,9 +1,8 @@
 import { FC } from 'react';
-interface StatusIconProps {
+export interface StatusIconProps {
     className?: string | string[];
     variant?: 'active' | 'inactive' | 'warning';
     size?: 'tiny' | 'medium' | 'big';
     tooltip?: string;
 }
 export declare const StatusIcon: FC<StatusIconProps>;
-export {};
