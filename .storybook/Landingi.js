c6b20e5bd9507b6ffd7882c03971ec5b3932ccdd
Bartek Kochanowicz <89905016+bartlomiej-kochanowicz-landingi@users.noreply.github.com>
2023-01-27 09:08:12 +0100

    Status icon safari fix v2 (#439)

---
+++ .storybook/Landingi.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming'
+
+export default create({
+  base: 'light',
+  brandTitle: 'Landingi',
+  brandUrl: 'https://landingi.com/',
+  brandImage:
+    'https://assets.landingi.com/wp-content/uploads/2021/01/28120318/landingi-logo.jpg',
+  brandTarget: '_self'
+})
