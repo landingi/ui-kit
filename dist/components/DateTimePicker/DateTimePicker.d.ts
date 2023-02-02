245797285d9ce10d9c680a86852236ab0631bcae
Bartlomiej Kochanowicz <bartlomiej.kochanowicz@landingi.com>
2023-02-02 19:57:41 +0100

    build

---
+++ dist/components/DateTimePicker/DateTimePicker.d.ts
@@ -0,0 +1,14 @@
+import './DateTimePicker.scss';
+import { FC } from 'react';
+export interface DateTimePickerProps {
+    setDate: (startDate?: Date, endDate?: Date) => void;
+    minDate?: string | Date;
+    maxDate?: string | Date;
+    oneDatePicker?: boolean;
+    selectedDateCalendar?: Date;
+    showMonthAndYearPickers?: boolean;
+    i18n: {
+        apply: string;
+    };
+}
+export declare const DateTimePicker: FC<DateTimePickerProps>;
