951905a4ea5bd8148e92f7e30a660c4ab51d98c5
Bartek Kochanowicz <89905016+bartlomiej-kochanowicz-landingi@users.noreply.github.com>
2023-02-02 14:15:48 +0100

    [APP-1150]DateTimePicker typescript (#447)

--- src/components/DateTimePicker/DateTimePicker.test.tsx
+++ src/components/DateTimePicker/DateTimePicker.test.tsx
@@ -1,20 +1,27 @@
-import DateTimePicker from '@components/DateTimePicker'
+import { DateTimePicker } from '@components/DateTimePicker'
 import { fireEvent, render } from '@testing-library/react'
-import React from 'react'
+
+const i18n = {
+  apply: 'Apply'
+}
 
 const mockSetState = jest.fn()
 
 describe('<DateTimePicker /> test as default variant', () => {
   it('renders properly', () => {
-    render(<DateTimePicker setDate={mockSetState} />)
+    render(<DateTimePicker setDate={mockSetState} i18n={i18n} />)
   })
 
   it('renders properly with min date', () => {
-    render(<DateTimePicker setDate={mockSetState} minDate='2021/11/10' />)
+    render(
+      <DateTimePicker setDate={mockSetState} minDate='2021/11/10' i18n={i18n} />
+    )
   })
 
   it('call setState on apply', () => {
-    const { getByTestId } = render(<DateTimePicker setDate={mockSetState} />)
+    const { getByTestId } = render(
+      <DateTimePicker setDate={mockSetState} i18n={i18n} />
+    )
 
     const applyButton = getByTestId('apply-button')
 
@@ -26,6 +33,6 @@ describe('<DateTimePicker /> test as default variant', () => {
 
 describe('<DateTimePicker /> test as one day picker', () => {
   it('renders properly', () => {
-    render(<DateTimePicker setDate={mockSetState} oneDatePicker />)
+    render(<DateTimePicker setDate={mockSetState} oneDatePicker i18n={i18n} />)
   })
 })
