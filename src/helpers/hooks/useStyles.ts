93a811de54d01b7652ba11842821098208aa7bbc
Bartlomiej Kochanowicz <bartlomiej.kochanowicz@landingi.com>
2023-03-01 11:00:46 +0100

    use Styles ts

--- src/helpers/hooks/useStyles.ts
+++ src/helpers/hooks/useStyles.ts
@@ -1,10 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable no-restricted-syntax */
+
 /**
  * useStyles - hook to extract classes from CSS modules object or provide custom one's
  * @param {object} styles - object with CSS modules styles
  * @param {string | array} classNames - custom classes from outside of component
  * @returns {string} An string with extracted class names
  */
-export const useStyles = (styles, classNames = '') => {
+export const useStyles = (
+  styles: Record<string, boolean>,
+  classNames: string | string[] = ''
+) => {
   let classes = ''
 
   for (const [key, value] of Object.entries(styles)) {
