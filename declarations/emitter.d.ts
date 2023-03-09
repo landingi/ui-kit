298850927126e179686ff695660e5c09e2e2163e
Bartlomiej Kochanowicz <bartlomiej.kochanowicz@landingi.com>
2023-03-09 08:59:31 +0100

    build

--- declarations/emitter.d.ts
+++ declarations/emitter.d.ts
@@ -1,10 +1,5 @@
 import TinyEmitter from 'tiny-emitter'
 
-declare module '*.scss' {
-  const content: Record<string, string>
-  export default content
-}
-
 declare module 'tiny-emitter' {
   const type: ReturnType<TinyEmitter>
 
