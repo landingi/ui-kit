e8666afffc1f45e6614fd6fefc92d89cd5764b71
Jarosław Kucharski <72187174+jaroslawkucharski-landingi@users.noreply.github.com>
2023-04-05 09:11:09 +0200

    storybook updated

--- src/components/PerfectDropdownSelect/PerfectDropdownSelect.stories.tsx
+++ src/components/PerfectDropdownSelect/PerfectDropdownSelect.stories.tsx
@@ -116,3 +116,35 @@ WithLoader.args = {
   },
   isLoading: true
 }
+
+export const ListItemWithTooltip = Template.bind({})
+
+ListItemWithTooltip.args = {
+  formikKey: 'test-dropdown',
+  value: 'test1',
+  options: [
+    { label: 'label1', value: 'test1', tooltip: 'Tooltipem content' },
+    { label: 'label2', value: 'test2' }
+  ],
+  label: 'Custom label for dropdown',
+  onChange: () => {}
+}
+
+export const DisaledListItem = Template.bind({})
+
+DisaledListItem.args = {
+  formikKey: 'test-dropdown',
+  value: 'test1',
+  options: [
+    { label: 'label1', value: 'test1' },
+    { label: 'label2', value: 'test2', disabled: true },
+    {
+      label: 'label2',
+      value: 'test2',
+      disabled: true,
+      tooltip: 'Tooltipem content'
+    }
+  ],
+  label: 'Custom label for dropdown',
+  onChange: () => {}
+}
