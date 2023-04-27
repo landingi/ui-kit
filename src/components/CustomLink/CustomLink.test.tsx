7561d7611f23558cb767d602d01a79104a1bbb26
Bartek Kochanowicz <89905016+bartlomiej-kochanowicz-landingi@users.noreply.github.com>
2023-04-27 09:45:21 +0200

    [APP-1386]Add bold to custom link and BoxOutlive hover behavior (#483)
    Jira: https://landingi.atlassian.net/jira/software/projects/APP/boards/110?selectedIssue=APP-1386
    
    Cursor pointer in BoxOutline component is active when hover props is provided.
    
    CustomLink has new props 'bold' - tested

--- src/components/CustomLink/CustomLink.test.tsx
+++ src/components/CustomLink/CustomLink.test.tsx
@@ -6,15 +6,23 @@ import { fireEvent, render } from '@testing-library/react'
 describe('<CustomLink/> tests', () => {
   const props = { label: 'Custom link' }
 
-  it('<CustomLink/> mout', () => {
+  it('mount', () => {
     render(<CustomLink {...props} />)
   })
 
-  it('<CustomLink/> mout & simulate click', () => {
+  it('mount & simulate click', () => {
     const { getByTestId } = render(<CustomLink {...props} />)
 
     const customLink = getByTestId('custom-link')
 
     fireEvent.click(customLink)
+  })
+
+  it('has bold', () => {
+    const { getByTestId } = render(<CustomLink bold {...props} />)
+
+    const customLink = getByTestId('custom-link')
+
+    expect(customLink).toHaveClass('custom-link--bold')
   })
 })
