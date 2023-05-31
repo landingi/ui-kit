c97a6c8dcd8a13b626f5635430b36934938472ed
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2023-05-31 11:35:49 +0200

    build

--- dist/components/Close/Close.d.ts
+++ dist/components/Close/Close.d.ts
@@ -3,6 +3,7 @@ interface CloseProps {
     className?: string | string[];
     onClick?: MouseEventHandler<HTMLSpanElement>;
     iconName?: string;
+    size?: 10 | 12 | 14 | 32;
     iconColor?: string;
 }
 export declare const Close: FC<CloseProps>;
