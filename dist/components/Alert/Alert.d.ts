0b91b29a55225aa89befe8972b9243c52b179e47
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2023-05-31 09:01:43 +0200

    build

--- dist/components/Alert/Alert.d.ts
+++ dist/components/Alert/Alert.d.ts
@@ -3,5 +3,6 @@ export interface AlertProps {
     children: ReactNode;
     className?: string | string[];
     type?: 'info' | 'success' | 'warning' | 'alert';
+    customIcon?: string;
 }
 export declare const Alert: FC<AlertProps>;
