7b12d6f77ad9ef037bbdf409b033b2b1c69cef02
lukaszochwat-landingi <67641152+lukaszochwat-landingi@users.noreply.github.com>
2023-06-05 08:51:57 +0200

    [GRO-3865]Add MultiSelect component (#491)
    Adds MultiSelect component that works as described here: https://landingi.atlassian.net/wiki/spaces/PROJ/pages/436928521/Proces+rejestracji+iteracja+2#Pytanie-4.1
    
    It is fully customizable, so:
    - screen that is shown when no search results are found can be any component that has props searchPhrase and addCustomValue. Those props let user add custom values when no searchPhrase is found.
    - initial options are customizable

---
+++ dist/components/Form/MultiSelect/MultiSelect.d.ts
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+declare type Value = string | number | null;
+interface Option {
+    label: string;
+    value: Value;
+    icon?: string;
+    selected?: boolean;
+}
+export interface EmptySearchResultsComponentProps {
+    addCustomOption: (option: Option) => void;
+    searchPhrase: string;
+}
+interface MultiSelectProps {
+    onChange: (formikKey: string, value: Value[]) => void;
+    id: string;
+    className?: string | string[];
+    formikKey: string;
+    'data-testid'?: string;
+    initialOptions: Option[];
+    values: Value[];
+    searcher: {
+        i18n: {
+            placeholder: string;
+        };
+    };
+    emptySearchResultsComponent: FC<EmptySearchResultsComponentProps>;
+}
+export declare const MultiSelect: FC<MultiSelectProps>;
+export {};
