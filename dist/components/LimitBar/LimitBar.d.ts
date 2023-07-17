3b377a0c12166cc63765ae67388833a081be26f6
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2023-07-17 08:37:25 +0200

    build

--- dist/components/LimitBar/LimitBar.d.ts
+++ dist/components/LimitBar/LimitBar.d.ts
@@ -1,21 +1,14 @@
 import { FC } from 'react';
 declare type LimitBarPadding = 'none' | 'tiny' | 'small' | 'medium' | 'regular';
-export interface CommonLimitBarProps {
+export interface LimitBarProps {
+    className?: string;
     padding?: LimitBarPadding;
     limit: number;
     quantity: number;
-    regularLimit?: number;
     limitText: string;
     tooltip?: string;
+    regularLimit?: number;
     tooltipInQuantity?: string;
-    shouldShowRegularLimit?: boolean;
-    className?: string;
 }
-declare type ShowRegulatLimitProps = {
-    regularLimit: number;
-    tooltipInQuantity: string;
-    shouldShowRegularLimit: boolean;
-};
-export declare type LimitBarProps = ShowRegulatLimitProps & CommonLimitBarProps;
 export declare const LimitBar: FC<LimitBarProps>;
 export {};
