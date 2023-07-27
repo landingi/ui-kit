99d4967bb1a5d60fea90b60c7be7d6627d2f4082
lukaszochwat-landingi <lukasz.ochwat@landingi.com>
2023-07-27 13:57:44 +0200

    fix

--- src/components/Form/MultiSelect/MultiSelect.tsx
+++ src/components/Form/MultiSelect/MultiSelect.tsx
@@ -235,34 +235,28 @@ export const MultiSelect: FC<MultiSelectProps> = ({
         )}
       </label>
 
-      <Spacer space='small' />
+      <div className={selectedOptionsStyles}>
+        {selectedOptions
+          .filter(({ selected }) => selected)
+          .map(({ label, value }) => (
+            <Badge
+              className={styles['icon-pack-badge']}
+              type='accent-8'
+              key={value}
+            >
+              <Row alignItems='center'>
+                <span className={styles['icon-pack-badge__text']}>{label}</span>
 
-      {!isEmpty(selectedOptions) && (
-        <div className={selectedOptionsStyles}>
-          {selectedOptions
-            .filter(({ selected }) => selected)
-            .map(({ label, value }) => (
-              <Badge
-                className={styles['icon-pack-badge']}
-                type='accent-8'
-                key={value}
-              >
-                <Row alignItems='center'>
-                  <span className={styles['icon-pack-badge__text']}>
-                    {label}
-                  </span>
-
-                  <span
-                    className={styles['icon-pack-badge__close']}
-                    onClick={() => removeValue(value)}
-                  >
-                    <Icon icon='icon-remove' color='white' />
-                  </span>
-                </Row>
-              </Badge>
-            ))}
-        </div>
-      )}
+                <span
+                  className={styles['icon-pack-badge__close']}
+                  onClick={() => removeValue(value)}
+                >
+                  <Icon icon='icon-remove' color='white' />
+                </span>
+              </Row>
+            </Badge>
+          ))}
+      </div>
     </div>
   )
 }
