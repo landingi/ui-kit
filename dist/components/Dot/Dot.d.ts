d4db4896574431f1f8ebedf9b7c17c4a778b9497
Jarosław Kucharski <72187174+jaroslawkucharski@users.noreply.github.com>
2023-08-08 15:25:38 +0200

    [APP-1721]Update Dot component (#523)
    Updated label prop type in the Dot component.

--- dist/components/Dot/Dot.d.ts
+++ dist/components/Dot/Dot.d.ts
@@ -1,7 +1,7 @@
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 interface DotProps {
     variant?: 'brand' | 'alert' | 'warning' | 'success' | 'info' | 'pending' | 'accent-1' | 'accent-2' | 'accent-3' | 'accent-4' | 'accent-5' | 'accent-6' | 'accent-7';
-    label?: string;
+    label?: ReactNode | string;
     className?: string | string[];
 }
 export declare const Dot: FC<DotProps>;
