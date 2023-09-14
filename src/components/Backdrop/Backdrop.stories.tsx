0920c17177cb52b06a4c61e63e18ecf6ffba7b80
Natalia <natalia.pietrzak@landingi.com>
2023-09-14 09:52:40 +0200

    Backdrop story

--- src/components/Backdrop/Backdrop.stories.tsx
+++ src/components/Backdrop/Backdrop.stories.tsx
@@ -1,31 +1,45 @@
-import { Backdrop } from '@components/Backdrop'
-import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { Backdrop, BackdropProps } from '@components/Backdrop'
+import { Meta, StoryFn } from '@storybook/react'
 
 export default {
   title: 'Backdrop',
   component: Backdrop
-} as ComponentMeta<typeof Backdrop>
+} as Meta<typeof Backdrop>
 
-const Template: ComponentStory<typeof Backdrop> = args => (
-  <div>
-    <Backdrop {...args} />
-    <div
-      style={{
-        width: 100,
-        height: 100,
-        background: 'red',
-        position: 'relative',
-        zIndex: 5,
-        display: 'inline-block'
-      }}
-    >
-      My z-index is 5
+const Template: StoryFn<typeof Backdrop> = (
+  args: BackdropProps & {
+    redBlockZIndex: number
+  }
+) => {
+  const { redBlockZIndex } = args
+  return (
+    <div>
+      <Backdrop {...args} />
+      <div
+        style={{
+          width: 100,
+          height: 100,
+          background: 'red',
+          position: 'relative',
+          zIndex: redBlockZIndex,
+          display: 'inline-block'
+        }}
+      >
+        My z-index is {redBlockZIndex}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export const Basic = Template.bind({})
+export const withCustomZIndex = Template.bind({})
 
 Basic.args = {
-  zIndex: '4'
+  zIndex: '4',
+  redBlockZIndex: 5
+}
+
+withCustomZIndex.args = {
+  redBlockZIndex: 15,
+  customZIndex: 10
 }
