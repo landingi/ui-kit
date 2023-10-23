d1ce7e052458e212a22967c095ed4a6943b350de
lukaszochwat-landingi <lukasz.ochwat@landingi.com>
2023-10-23 08:20:15 +0200

    [GRO-4549]Add classname to Accordion

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,4 +1,5 @@
 import { generateFakeUuid } from '@helpers/data'
+import { useStyles } from '@helpers/hooks/useStyles'
 import { FC, memo, ReactNode } from 'react'
 
 import { AccordionItem } from './AccordionItem'
@@ -11,23 +12,28 @@ export interface AccordionProps {
   }[]
   padding?: 'none' | 'small' | 'medium'
   isBox?: boolean
+  className?: string | string[]
 }
 
 export const Accordion: FC<AccordionProps> = memo<AccordionProps>(
-  ({ data, padding = 'medium', isBox = false }: AccordionProps) => (
-    <div>
-      {data.map(({ title, description, content }) => (
-        <AccordionItem
-          key={generateFakeUuid()}
-          title={title}
-          description={description}
-          content={content}
-          padding={padding}
-          isBox={isBox}
-        />
-      ))}
-    </div>
-  )
+  ({ data, padding = 'medium', isBox = false, className }: AccordionProps) => {
+    const styles = useStyles({}, className)
+
+    return (
+      <div className={styles}>
+        {data.map(({ title, description, content }) => (
+          <AccordionItem
+            key={generateFakeUuid()}
+            title={title}
+            description={description}
+            content={content}
+            padding={padding}
+            isBox={isBox}
+          />
+        ))}
+      </div>
+    )
+  }
 )
 
 Accordion.displayName = 'Accordion'
