f958408e60dc8373355a8d81044d294ad07d6f65
Łukasz Ochwat <67641152+lukaszochwat-landingi@users.noreply.github.com>
2023-10-23 10:27:29 +0200

    [GRO-4549]Add className & isOpenByDefault props to Accordion (#567)
    Add className prop to Accordion, as it was lacking since rewriting it to ui-kit. Also add isOpenByDefault prop. By default, all accordion items were opened - it isn't always desired behaviour. Now it is possible to set whether accordion items should be opened by default or not.

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,4 +1,5 @@
 import { generateFakeUuid } from '@helpers/data'
+import { useStyles } from '@helpers/hooks/useStyles'
 import { FC, memo, ReactNode } from 'react'
 
 import { AccordionItem } from './AccordionItem'
@@ -11,23 +12,36 @@ export interface AccordionProps {
   }[]
   padding?: 'none' | 'small' | 'medium'
   isBox?: boolean
+  isOpenByDefault?: boolean
+  className?: string | string[]
 }
 
 export const Accordion: FC<AccordionProps> = memo<AccordionProps>(
-  ({ data, padding = 'medium', isBox = false }: AccordionProps) => (
-    <div>
-      {data.map(({ title, description, content }) => (
-        <AccordionItem
-          key={generateFakeUuid()}
-          title={title}
-          description={description}
-          content={content}
-          padding={padding}
-          isBox={isBox}
-        />
-      ))}
-    </div>
-  )
+  ({
+    data,
+    padding = 'medium',
+    isBox = false,
+    isOpenByDefault = true,
+    className
+  }: AccordionProps) => {
+    const styles = useStyles({}, className)
+
+    return (
+      <div className={styles}>
+        {data.map(({ title, description, content }) => (
+          <AccordionItem
+            key={generateFakeUuid()}
+            title={title}
+            description={description}
+            content={content}
+            padding={padding}
+            isBox={isBox}
+            isOpenByDefault={isOpenByDefault}
+          />
+        ))}
+      </div>
+    )
+  }
 )
 
 Accordion.displayName = 'Accordion'
