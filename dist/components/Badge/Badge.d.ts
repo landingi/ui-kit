01b682c1bfa57289aab9d8a801548c99b998e0bc
lukaszochwat-landingi <lukasz.ochwat@landingi.com>
2023-11-22 10:33:56 +0100

    [GRO-4565]Add new variant to Badge and Paragraph

--- dist/components/Badge/Badge.d.ts
+++ dist/components/Badge/Badge.d.ts
@@ -2,7 +2,7 @@ import { FC, ReactNode } from 'react';
 export interface BadgeProps {
     children: ReactNode;
     className?: string | string[];
-    type?: 'warning' | 'alert' | 'pending' | 'success' | 'info' | 'paid' | 'open' | 'canceled' | 'ai' | 'accent-1' | 'accent-2' | 'accent-3' | 'accent-4' | 'accent-5' | 'accent-6' | 'accent-7' | 'accent-8' | 'green-with-border';
+    type?: 'warning' | 'alert' | 'pending' | 'success' | 'info' | 'paid' | 'open' | 'canceled' | 'ai' | 'accent-1' | 'accent-2' | 'accent-3' | 'accent-4' | 'accent-5' | 'accent-6' | 'accent-7' | 'accent-8' | 'green-with-border' | 'info-with-border';
     tooltip?: ReactNode;
     isIndicator?: boolean;
 }
