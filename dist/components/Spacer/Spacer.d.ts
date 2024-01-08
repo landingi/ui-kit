39b4875a020a66de8a9156517f03549dd31f6dac
lukaszochwat-landingi <lukasz.ochwat@landingi.com>
2024-01-08 08:28:40 +0100

    build

--- dist/components/Spacer/Spacer.d.ts
+++ dist/components/Spacer/Spacer.d.ts
@@ -1,7 +1,6 @@
 import { FC } from 'react';
-interface SpacerProps {
+export interface SpacerProps {
     className?: string | string[];
     space?: 'mini' | 'tiny' | 'small' | 'medium' | 'regular' | 'large' | 'big' | 'huge';
 }
 export declare const Spacer: FC<SpacerProps>;
-export {};
